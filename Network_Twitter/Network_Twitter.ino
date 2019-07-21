62c0c1a9504176381aa32e724255aaaaf93cd8dd
chan myong park <pcm0028@naver.com>
2019-07-21 22:23:01 +0900

    Add files via upload

---
+++ Network_Twitter/Network_Twitter.ino
@@ -0,0 +1,64 @@
+#include <Wire.h>
+#include <Twitter.h>
+#include <SPI.h>
+#include <WiFi.h>
+
+int locked = 0;
+
+char ssid[] = "ollehEgg_419";  //연결할 무선 네트워크 이름을 입력해주세요.
+char pass[] = "info10019";      //비밀번호를 입력해주세요.
+
+Twitter twitter("3636103272-L5urY3iQPaSf3vBVU9oE8TvpyPa7CnU8xnKeHzY"); 
+
+char msg[] = "For someone came to my house ! WTF!!";
+int num =0;
+void setup() {
+  Serial.begin(9600);
+  Wire.begin(4);                
+  Wire.onReceive(receiveEvent); 
+/*시작*/
+
+  delay(1000);
+  WiFi.begin(ssid, pass); 
+  
+  Serial.begin(9600);
+
+  /*종료*/
+}
+
+void loop() {
+   
+   if(locked==1){
+     delay(500);
+     char tmp[10];
+     sprintf(tmp," %d",num);
+     char str[100];
+     strcpy(str, msg);
+     strcat(str, tmp);
+    
+     
+     Serial.println("connecting ...");  //와이파이 연결후 메세지 보내기 시작 전에 확인
+  
+      if (twitter.post(str)) {           //메세지를 전송 확인
+ 
+      int status = twitter.wait(&Serial);  //메세지 보내기가 끝난 후 트위터로부터 상태전달 받음
+      if (status == 200) {                 //200을 전달받으면 
+        Serial.println("OK.");             //성공된 것이므로 시리얼 모니터에 OK출력
+      } else {                             //아닐 경우 실패 메세지와 전달받은 값 출력
+        Serial.print("failed : code ");
+        Serial.println(status);
+      }
+    } else {                              //연결부터 실패 했을 경우
+      Serial.println("connection failed.");
+    }
+    locked=0;
+    num++;
+    delay(10000);
+   }
+}
+
+void receiveEvent(int howMany)
+{
+  locked = Wire.read();    
+  Serial.println(locked);        
+}
---
+++ Sensor_Password/Sensor_Password.ino
@@ -0,0 +1,100 @@
+#include<Wire.h>
+
+const int sensor = A0;
+const int PIRSensor = 11;
+
+const int ledR = A1;
+const int ledY = A2;
+const int ledB = A3;
+
+const int changeButton = 12;
+const int selectButton = 13;
+
+int selectNum[2] = {};
+int selectCount = 0;
+
+int password[2] = {1,2};
+bool unlocked = false;
+
+int segPins[]={2,3,4,5,6,7,8,9};
+byte digits[10][8]={
+  {0,0,0,0,0,0,1,1},
+  {1,0,0,1,1,1,1,1},
+  {0,0,1,0,0,1,0,1},
+  {0,0,0,0,1,1,0,1},
+  {1,0,0,1,1,0,0,1},
+  {0,1,0,0,1,0,0,1},
+  {0,1,0,0,0,0,0,1},
+  {0,0,0,1,1,0,1,1},
+  {0,0,0,0,0,0,0,1},
+  {0,0,0,0,1,0,0,1}
+};
+
+int countNum=0;
+
+int sensorVal=0;
+
+int benchmarkLight = 20;
+
+void setup() {
+  Serial.begin(9600);
+  Wire.begin();
+  pinMode(changeButton, INPUT);
+  pinMode(selectButton, INPUT);
+  pinMode(ledR, OUTPUT);
+  pinMode(ledY, OUTPUT);
+  pinMode(ledB, OUTPUT);
+  pinMode(PIRSensor, INPUT);
+  for (int i = 0; i < 8; i++)  pinMode(segPins[i], OUTPUT);
+}
+
+void loop() {
+  if(digitalRead(changeButton) == HIGH && digitalRead(selectButton) == LOW){
+    countNum++;
+    countNum %= 10;
+    delay(250);
+  }
+
+  else if(digitalRead(changeButton) == LOW && digitalRead(selectButton) == HIGH){
+    if (selectCount<2){
+      selectNum[selectCount] = countNum;
+      selectCount++;
+      countNum = 0;
+      delay(400);
+    }
+  }
+
+  for (int i = 0; i < 8; i++)   digitalWrite(segPins[i], digits[countNum][i]);
+
+  if (selectCount == 1)    digitalWrite(ledY, HIGH);
+  else    digitalWrite(ledY, LOW);
+
+  if (selectCount == 2){
+      if ((selectNum[0] == password[0]) && (selectNum[1] == password[1]))   unlocked = true;
+      else unlocked = false;
+
+      selectCount = 0;
+  }
+
+  sensorVal = analogRead(sensor);
+Serial.println(sensorVal);
+
+  if (unlocked){
+    digitalWrite(ledB, HIGH);
+    digitalWrite(ledR, LOW);
+  } else{
+      digitalWrite(ledB, LOW);
+      digitalWrite(ledR, HIGH);
+    }
+  Serial.println(sensorVal);
+  if (!unlocked && (benchmarkLight > sensorVal) && digitalRead(PIRSensor) == HIGH){
+    Wire.beginTransmission(4);
+    Wire.write(1); 
+    Wire.endTransmission();
+  } else{
+      Wire.beginTransmission(4);
+      Wire.write(0); 
+      Wire.endTransmission();
+    }
+    
+}
